c04459997ce82ec15e55d014d3d017b6b2c64afd
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-09-16 04:41:20 +0300

    add user id on attendance register and profile

--- app/controllers/user_details_controller.rb
+++ app/controllers/user_details_controller.rb
@@ -1,7 +1,6 @@
 class UserDetailsController < ApplicationController
     def create
-        current_user = session[:user_id]
-        user_details = current_user.UserDetail.create!(user_payment_details_params)
+        user_details = UserDetail.create!(user_payment_details_params)
         render json: user_details, status: :created
     end
 
@@ -19,7 +18,7 @@ class UserDetailsController < ApplicationController
     private
 
     def user_payment_details_params
-        params.permit(:name_on_account,:bank_name,:account_number,:bank_code,:branch_code,:telephone_number)
+        params.permit(:user_id,:name_on_account,:bank_name,:account_number,:bank_code,:branch_code,:telephone_number)
     end 
 
     
